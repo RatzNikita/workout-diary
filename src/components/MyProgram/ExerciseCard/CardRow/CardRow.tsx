cbc8442368506a50d29f903dfd85ac830b5df559
Рац Никита Дмитриевич <Nikita.Rats@vaz.ru>
2023-06-19 15:13:54 +0400

    Вынес строки в карточке в отдельный компонент

---
+++ src/components/MyProgram/ExerciseCard/CardRow/CardRow.tsx
@@ -0,0 +1,64 @@
+import {IconButton, List, ListItem, ListItemText, TextField, Typography} from "@mui/material";
+import styles from "./CardRow.module.css";
+import React from "react";
+import {BuiltExerciseType} from "@component/components/MyProgram/ProgramConstructor/ProgramConstructor";
+import CreateIcon from '@mui/icons-material/Create';
+import DoneIcon from "@mui/icons-material/Done";
+
+interface CardRowProps {
+    exercise: BuiltExerciseType,
+    submitWeight: (weight: number, ex: BuiltExerciseType) => void,
+}
+
+
+enum Mode {
+    change,
+    view
+}
+
+export const CardRow = ({exercise, submitWeight}: CardRowProps) => {
+
+    const [mode, setMode] = React.useState(Mode.view)
+    const [weight, setWeight] = React.useState(exercise.weight.toString())
+
+    const handleChangeMode = (mode: Mode) => {
+        setMode(mode)
+    }
+
+    const handleChangeWeight = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setWeight(e.target.value)
+    }
+
+    const handleSubmitChange = () => {
+        submitWeight(+weight, exercise)
+        setMode(Mode.view)
+    }
+
+    return (
+        <List key={exercise.exercise.name}>
+            <ListItem className={styles.cardItem}
+                      secondaryAction={
+                          mode !== Mode.change
+                              ? <div className={styles.rowAction}>
+                                  <Typography variant='h6'>{`${exercise.weight}кг`}</Typography>
+                                  <IconButton className={styles.actionButton}
+                                              onClick={() => handleChangeMode(Mode.change)}><CreateIcon/></IconButton>
+                              </div>
+                              : <div className={styles.rowAction}>
+                                  <TextField inputProps={{maxLength: 4, className: styles.cardInput}}
+                                             variant="standard"
+                                             value={weight}
+                                             onChange={handleChangeWeight}>
+                                  </TextField>
+                                  <IconButton className={styles.actionButton}
+                                              onClick={handleSubmitChange}><DoneIcon/></IconButton>
+                              </div>
+                      }
+            >
+                <ListItemText className={styles.cardItemText}
+                              primary={exercise.exercise.name}
+                              secondary={`${exercise.sets}x${exercise.reps}`}/>
+            </ListItem>
+        </List>
+    )
+}
--- src/components/MyProgram/ExerciseCard/ExerciseCard.tsx
+++ src/components/MyProgram/ExerciseCard/ExerciseCard.tsx
@@ -2,26 +2,26 @@ import {Box, Button, List, ListItem, ListItemText, TextField, Typography} from "
 import {BuiltExerciseType, Workout} from "@component/components/MyProgram/ProgramConstructor/ProgramConstructor";
 import styles from './ExerciseCard.module.scss'
 import React from "react";
+import {useAppDispatch} from "@component/hooks/hooks";
+import {setWorkout} from "@component/store/reducers/mainSlice";
+import {CardRow} from "@component/components/MyProgram/ExerciseCard/CardRow/CardRow";
 
-
-enum Mode {
-    change = 'change',
-    view = 'view'
-}
 
 export const ExerciseCard = ({exercises, day}: Workout) => {
 
 
-    const [mode, setMode] = React.useState(Mode.view)
+    const dispatch = useAppDispatch();
 
-    const handleChangeMode = () => {
-        setMode(Mode.change)
-    }
 
-    const handleChangeWeight = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>, ex: BuiltExerciseType) => {
-        if (e.target.value) {
+    const handleChangeWeight = (weight: number, ex: BuiltExerciseType) => {
+        const newExercises = exercises.map(exe => {
+            if (exe.exercise === ex.exercise) {
+                return {...exe,weight: weight}
+            }
+            return exe;
+        })
+        dispatch(setWorkout({exercises: newExercises, day: day}))
 
-        }
     }
 
     return (
@@ -32,25 +32,12 @@ export const ExerciseCard = ({exercises, day}: Workout) => {
             <div className={styles.cardContent}>
                 {exercises.map(ex => {
                     return (
-                        <List key={ex.exercise.name}>
-                            <ListItem className={styles.cardItem}
-                                      secondaryAction={
-                                          mode !== Mode.change
-                                              ? <Typography variant='h6'>{`${ex.weight}кг`}</Typography>
-                                              : <TextField className={styles.cardInput}
-                                                           variant="standard" value={ex.weight}
-                                                           onChange={e => handleChangeWeight(e, ex)}></TextField>}
-                            >
-                                <ListItemText className={styles.cardItemText}
-                                              primary={ex.exercise.name}
-                                              secondary={`${ex.sets}x${ex.reps}`}/>
-                            </ListItem>
-                        </List>
-
+                        <CardRow key={ex.exercise.name}
+                                 exercise={ex}
+                                 submitWeight={handleChangeWeight}/>
                     )
                 })}
             </div>
-            <Button onClick={handleChangeMode}>Изменить</Button>
         </Box>
     )
 }
