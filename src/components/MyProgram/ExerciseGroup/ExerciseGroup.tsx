a6127b360b8911f9ee27fe91a44b01099bd3054e
Рац Никита Дмитриевич <Nikita.Rats@vaz.ru>
2023-06-21 13:53:43 +0400

    вынес часть логики из ExerciseList в отдельный компонент ExerciseGroup

---
+++ src/components/MyProgram/ExerciseGroup/ExerciseGroup.tsx
@@ -0,0 +1,32 @@
+import styles from "./ExerciseGroup.module.css";
+import {Collapse, ListItemButton, ListItemText} from "@mui/material";
+import {ExpandLess, ExpandMore} from "@mui/icons-material";
+import React, {ReactElement} from "react";
+
+interface ExerciseGroupProps {
+    value: string,
+    children: ReactElement,
+}
+
+
+export const ExerciseGroup = ({value, children}: ExerciseGroupProps) => {
+
+    const [open, setOpen] = React.useState(false)
+
+    const handleOpen = () => {
+            setOpen(!open)
+    }
+    return (
+        <div className={styles.groupContainer}>
+            <ListItemButton onClick={handleOpen} className={styles.listItem}>
+                <ListItemText primary={value}/>
+                {open ? <ExpandLess/> : <ExpandMore/>}
+            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit orientation="horizontal">
+                {children}
+            </Collapse>
+        </div>
+
+
+    )
+}
