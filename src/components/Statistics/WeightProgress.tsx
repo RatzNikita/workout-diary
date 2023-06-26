44e276144a43b33ca0c34ab6ccd6dfbb72df701b
Рац Никита Дмитриевич <Nikita.Rats@vaz.ru>
2023-06-26 15:29:18 +0400

    добавил библиотеку recharts для графиков чтобы просматривать прогрессию весов

---
+++ src/components/Statistics/WeightProgress.tsx
@@ -0,0 +1,50 @@
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import styles from './WeightProgress.module.css'
+
+const data = [
+    {name: '25 may', weight: 45},
+    {name: '30 may', weight: 47.5},
+    {name: '25 june', weight: 47.5},
+    {name: '25 june', weight: 50},
+    {name: '25 june', weight: 50},
+    {name: '25 june', weight: 55},
+    {name: '25 july', weight: 60},
+    {name: '25 may', weight: 45},
+    {name: '30 may', weight: 47.5},
+    {name: '25 june', weight: 47.5},
+    {name: '25 june', weight: 50},
+    {name: '25 june', weight: 50},
+    {name: '25 june', weight: 55},
+    {name: '25 july', weight: 60},
+    {name: '25 may', weight: 45},
+    {name: '30 may', weight: 47.5},
+    {name: '25 june', weight: 47.5},
+    {name: '25 june', weight: 50},
+    {name: '25 june', weight: 50},
+    {name: '25 june', weight: 55},
+    {name: '25 july', weight: 60},
+    {name: '25 may', weight: 45},
+    {name: '30 may', weight: 47.5},
+    {name: '25 june', weight: 47.5},
+    {name: '25 june', weight: 50},
+    {name: '25 june', weight: 50},
+    {name: '25 june', weight: 55},
+    {name: '25 july', weight: 60},
+];
+
+
+export const WeightProgress = () => {
+
+    return (
+        <div className={styles.container}>
+        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+            <Line type="monotone" dataKey="weight" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis minTickGap={20} dataKey={"name"}/>
+            <YAxis  type="number" domain={['dataMin - 10', 'dataMax + 10']} />
+            <Tooltip />
+        </LineChart>
+        </div>
+    )
+
+}
